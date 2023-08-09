03fd50ad7989b31ab702fedaf9c71ded58ce6269
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-08-09 21:18:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  if (username === "user" && password === "password") {
+  if (username === "admin@sinh" && password === "admin@sinh") {
     window.location.href = "welcome.html";
   } else {
     alert("Invalid credentials. Please try again.");
